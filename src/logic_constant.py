08ffd379d8774ff8c31b56e1505e4085fd636f7b
ToojO <florian.d-agostino@laplateforme.io>
2026-01-27 11:05:48 +0100

    Initial Commit
    [Add] logic.py / game_function.py logic_constant.py

---
+++ src/logic_constant.py
@@ -0,0 +1,22 @@
+
+
+# --- CONSTANT ---
+
+KEYBOARD = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",";",":","!","$"]
+
+LIFE_MAX = 3
+SCORE_ADD = 1
+
+# Time
+FPS = 60
+TIME_INIT_LETTERS = 2.0
+SPAW_INIT = 1.0
+SPEED_RATIO = 0.80
+
+
+# Var
+
+letters = []
+spawn_timer = 0.0
+lives = LIFE_MAX
+score = 0
